308e0dcdcd3f142565bcec085c556209f6b2ba60
Nazar Kosiv <nazarkosiv94@gmail.com>
2018-11-16 18:17:20 +0200

    .gitignore was changed

--- app.js
+++ app.js
@@ -7,8 +7,7 @@ const errorController = require('./controllers/error');
 
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+app.set('view engine', 'pug');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
